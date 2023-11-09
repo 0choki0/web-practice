00dd28df0e964b139fcb98d481deb739fb8c3210
jiho <19wlgh95@naver.com>
2023-11-09 15:23:27 +0900

    porfile 기능 구현

--- insta/accounts/views.py
+++ insta/accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserFreationForm
+from .forms import CustomUserFreationForm, CustomAuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from .models import User
 
 # Create your views here.
 
@@ -17,3 +20,32 @@ def signup(request):
     }
 
     return render(request, 'accounts_form.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('posts:index')
+    else:
+        form = CustomAuthenticationForm()
+    
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts_form.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:login')
+    
+def profile(request, username):
+    user_info = User.objects.get(username=username)
+
+    context = {
+        'user_info': user_info,
+    }
+
+    return render(request, 'profile.html', context)
